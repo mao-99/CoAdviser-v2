5142d8f36a70c9278e3a4607370a7f7de15e9edd
Mubarak Odufade <mubarakodufade@gmail.com>
2024-01-15 19:12:23 -0500

    Added courses array, and loop for checkboxes

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
   import "./app.css";
+  import { courses } from "./lib/courses";
+    import type { Course } from "./lib/types";
+
+    // Function to handle checkbox changes
+    function handleCheckboxChange(course:Course) {
+      // Update the 'taken' property of the course based on checkbox state
+      course.taken = !course.taken;
+    }
 </script>
 
 <main>
+  <!-- Render checkboxes for each course -->
+{#each courses as course (course.id)}
+<div>
+  <label>
+    <input type="checkbox" bind:checked={course.taken} on:change={() => handleCheckboxChange(course)} />
+    {course.name}
+  </label>
+</div>
+{/each}
 </main>
---
+++ src/lib/courses.ts
@@ -0,0 +1,28 @@
+import type { Course } from "./types";
+
+export const courses: Course[] = [
+    {
+        id: 1,
+        name: "SPN 111",
+        credits: 3,
+        taken: true,
+        prereqs: [],
+        coreqs: [],
+    },
+    {
+        id: 1,
+        name: "CHN 111",
+        credits: 3,
+        taken: true,
+        prereqs: [],
+        coreqs: [],
+    },
+    {
+        id: 1,
+        name: "CHM 111",
+        credits: 3,
+        taken: true,
+        prereqs: [],
+        coreqs: [],
+    }
+]
